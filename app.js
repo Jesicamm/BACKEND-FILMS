91685fa3dc7b1a02e787364c4b5748cf220cf380
keika <jesicamorocho.m@gmail.com>
2021-02-18 19:50:49 +0100

    created server

---
+++ app.js
@@ -0,0 +1,9 @@
+//Requerimos express a una constante.
+
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Listening at ${port}`));
