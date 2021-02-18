c3bc3cb25be89fa81226e281df976f28a9061ccd
keika <jesicamorocho.m@gmail.com>
2021-02-18 20:17:43 +0100

    user and film model defined

---
+++ Models/user.js
@@ -0,0 +1,17 @@
+class User {
+    constructor(name, surname, idUser, dni, email, password, phone, address, payment) {
+        this.name = name,
+            this.surname = surname,
+            this.idUser = idUser,
+            this.dni = dni,
+            this.email = email,
+            this.password = password,
+            this.phone = phone,
+            this.address = adress,
+            this.payment = payment;
+    }
+
+
+}
+
+module.exports = User;
