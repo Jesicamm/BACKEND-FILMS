d6b074a38a73d2d4dbc3d819255bf883e72f7944
keika <jesicamorocho.m@gmail.com>
2021-02-20 12:01:15 +0100

    adding mongoose, bodyparser and method-override

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 //Requerimos express a una constante.
 
 const express = require('express');
-
 const app = express();
 
-const port = 3000;
+//Requerimos bodyparser & methodOverride (para parsear json y personalizar metodos http)
+
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+const router = express.Router();
+router.get('/', function(req, res) {
+    res.send('Hello WOrld')
+});
+app.use(router);
+
+const port = 8000;
 
 app.listen(port, () => console.log(`Listening at ${port}`));
